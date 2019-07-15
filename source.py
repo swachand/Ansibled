a637dbd6a020facb825c5c4ae7d478895f9c8c73
Maheshkrishna A G <maheshkrishnagopal@gmail.com>
2019-07-15 18:59:29 +0530

    Update source.py

--- source.py
+++ source.py
@@ -3,7 +3,7 @@ import sqlite3
 
 final_playbook = '---' + '\n' + '- name: Ansible Playbook' + '\n- hosts: '
 hosts = input("Please enter the host string or IP: ")
-final_playbook += 'hosts\n' + '- become: yes' + '\n' + '- become_user: root' + '\n\n' + '  tasks:\n'
+final_playbook += hosts + '\n' + '- become: yes' + '\n' + '- become_user: root' + '\n\n' + '  tasks:\n'
 user_input = input("Please enter the search keyword:")
 conn = sqlite3.connect("D:\\A\\ansiblator.db")
 res = conn.execute("SELECT name,description FROM MODULES WHERE name like ?", ('%'+user_input+'%',)).fetchall()
@@ -12,13 +12,16 @@ for i in res:
     result.append(i)
 
 for j in range(1, len(result)):
-    print(str(j) + ' - ' + result[j][0])
+    print(str(j) + '. ' + result[j][0] + ' - ' + result[j][1])
 
-row_num = int(input("Please select a module number from above: Ex: 12 :"))
-try:
-    print('You have chosen the Ansible Module "' + str(result[row_num][0]) + '"')
-    print("Description:", result[row_num][1])
-except IndexError:
-    print("Error - Please enter number within the limit!!!")
 
+def excepting():
+    row_num = int(input("\nPlease select a module number from above: Ex: 12 :"))
+    try:
+        print('\nYou have chosen the Ansible Module "' + str(result[row_num][0]) + '"')
+        print("Description:", result[row_num][1])
+    except IndexError:
+        print("\nError - Please enter number within the available limit!!! \n")
+        excepting()
+excepting()
 print(final_playbook + '\n...')
