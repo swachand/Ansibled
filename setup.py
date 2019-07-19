4be1292e4c7ba266c959359b3e564f1d9414af5d
Maheshkrishna A G <maheshkrishnagopal@gmail.com>
2019-07-19 14:57:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ansiblator-idiot_ag",
+    name="ansibled",
     version="0.0.1",
     author="Maheshkrishna A G",
     author_email="maheshkrishnagopal@gmail.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Maheshkrishna/Ansiblator",
+    url="https://github.com/Maheshkrishna/Ansibled",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
